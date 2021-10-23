55c35f98d1580cabfb88832b030ad1c4b7a254f6
嚴俊皓 <92283736+yen-chun-hao@users.noreply.github.com>
2021-10-24 02:59:43 +0800

    Update class07.java

--- class07.java
+++ class07.java
@@ -1,22 +1,21 @@
-class Cbox
+class CTest
 {
-    int length,width,height;
-}
-public class hw8_5
-{
-    public static void main(String arge[])
+    void test(int n)
     {
-        Cbox box;
-        box=new Cbox();
-        box.length=1;
-        box.width=1;
-        box.height=1;
-        int volume,surfaceArea,showData,showall;
-        volume=box.length*box.width*box.height;
-        surfaceArea=box.length*box.width*6;
-        System.out.println("volume= "+volume);
-        System.out.println("surfaceArea= "+surfaceArea);
-        System.out.println("showData= "+box.length+","+box.width+","+box.height);
-        System.out.println("showall= "+box.length+","+box.width+","+box.height+","+volume+","+surfaceArea);
+        if(n%2==1)
+        System.out.println("n="+n+",此數為奇數");
+        else if(n==0)
+        System.out.println("n="+n+",此數為0");
+        else
+        System.out.println("n="+n+",此數為偶數");
     }
 }
+public class hw8_7 {
+public static void main (String args[]){
+CTest a = new CTest();
+a.test(3);
+a.test(8);
+a.test(0);
+}
+}
+
