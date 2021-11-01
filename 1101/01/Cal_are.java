cdd70703604bb05f7580942dd20f522f350b4db4
嚴俊皓 <92283736+yen-chun-hao@users.noreply.github.com>
2021-11-01 12:15:31 +0800

    Add files via upload

---
+++ 1101/01/Cal_are.java
@@ -0,0 +1,12 @@
+class Cal_are
+{ 
+    double raduius = 10;
+    
+public static void main(String[] args){
+   
+    System.out.println("半徑 = "+10);
+    System.out.println("球形體積 = "+Math.round((4.0/3.0*Math.PI*10*10*10)*10.0/10.0));
+    System.out.println("長="+4+","+"寬="+5+","+"高="+6);
+    System.out.println("立方體表面積="+(4*5*2+4*6*2+5*6*2));
+}   
+}
